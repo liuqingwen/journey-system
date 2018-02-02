cd2e4a0421a53db933671f6b59c2f28a2689d718
刘庆文 <lqingw1207@qq.com>
2018-02-02 15:46:00 +0800

    Surefire 测试

---
+++ journey-test/src/test/java/com/journey/test/SurefireTest.java
@@ -0,0 +1,26 @@
+package com.journey.test;
+
+import org.junit.Test;
+
+/**
+ * @author liuqingwen
+ * @date 2018/2/2.
+ */
+public class SurefireTest {
+
+    @Test
+    public void test() {
+        System.out.println("测试1");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("测试2");
+    }
+
+    @Test
+    public void test3() {
+        System.out.println("测试3");
+    }
+
+}
