b89d7b124acdf44af74eca32ecd88cb6ef36fce7
刘庆文 <lqingw1207@qq.com>
2018-02-05 15:13:02 +0800

    lenth

--- journey-test/src/test/java/com/journey/test/SpringTest.java
+++ journey-test/src/test/java/com/journey/test/SpringTest.java
@@ -1,6 +1,9 @@
 package com.journey.test;
 
 import org.junit.Test;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
@@ -19,5 +22,29 @@ public class SpringTest {
 
     }
 
+    @Test
+    public void test2() {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'");
+        String message = (String) exp.getValue();
+        System.out.println(message);
+    }
+
+    @Test
+    public void test3() {
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression exp = parser.parseExpression("'Hello World'.concat('!')");
+        String message = (String) exp.getValue();
+        System.out.println(message);
+    }
+
+    @Test
+    public void test4() {
+        ExpressionParser parser = new SpelExpressionParser();
+//         invokes 'getBytes()'
+        Expression exp = parser.parseExpression("'Hello World'.bytes.length");
+        int message_length = (int)exp.getValue();
+        System.out.println(message_length);
+    }
 
 }
