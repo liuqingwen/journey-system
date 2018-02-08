f18c0d200e2a164ce49f638532ea5ab55eeab4a5
刘庆文 <lqingw1207@qq.com>
2018-02-08 15:46:57 +0800

    dubbo and md5

---
+++ journey-test/src/test/java/com/journey/test/visualization/Test.java
@@ -0,0 +1,18 @@
+package com.journey.test.visualization;
+
+import java.awt.*;
+
+/**
+ * @author liuqingwen
+ * @date 2018/2/8.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        Toolkit.getDefaultToolkit().beep();
+
+    }
+
+}
