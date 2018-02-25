4fd6cd29adad35c15b25de88acd5bbddc3e2f4e7
刘庆文 <lqingw1207@qq.com>
2018-02-25 09:54:18 +0800

    Redis 测试

--- journey-test/src/test/java/com/journey/test/Test.java
+++ journey-test/src/test/java/com/journey/test/Test.java
@@ -3,6 +3,7 @@ package com.journey.test;
 import com.alibaba.fastjson.JSON;
 import com.google.common.base.Strings;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
@@ -423,6 +424,16 @@ public class Test {
     public void test29() {
 
         System.out.println(String.format("%s", "我是好人"));
+
+    }
+
+    @org.junit.Test
+    public void test30() {
+
+        HashMap<Object, Object> objectObjectHashMap = Maps.newHashMap();
+        objectObjectHashMap.put("name", 1);
+        objectObjectHashMap.putIfAbsent("name", 0);
+        System.out.println(objectObjectHashMap.get("name"));
 
     }
 }
