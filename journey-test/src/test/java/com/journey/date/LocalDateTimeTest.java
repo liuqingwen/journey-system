79e910582b983df5d2d350097395473c0ecc4692
刘庆文 <lqingw1207@qq.com>
2018-03-19 21:30:19 +0800

    Java 8 LocalDateTime

--- journey-test/src/test/java/com/journey/date/LocalDateTimeTest.java
+++ journey-test/src/test/java/com/journey/date/LocalDateTimeTest.java
@@ -24,6 +24,23 @@ public class LocalDateTimeTest {
 
 //    Joda-Time
 
+//    方法大致功能
+//    of： 静态工厂方法，从组成部分中创建实例
+//    from： 静态工厂方法，尝试从相似对象中提取实例。from()方法没有of()方法类型安全
+//    now： 静态工厂方法，用当前时间创建实例
+//    parse： 静态工厂方法，总字符串解析得到对象实例
+//    get： 获取时间日期对象的部分状态
+//    is： 检查关于时间日期对象的描述是否正确
+//    with： 返回一个部分状态改变了的时间日期对象拷贝
+//    plus： 返回一个时间增加了的、时间日期对象拷贝
+//    minus： 返回一个时间减少了的、时间日期对象拷贝
+//    to： 把当前时间日期对象转换成另外一个，可能会损失部分状态
+//    at： 用当前时间日期对象组合另外一个，创建一个更大或更复杂的时间日期对象
+//    format： 提供格式化时间日期对象的能力
+
+//    原文链接
+//    http://www.blogjava.net/4cai/archive/2014/04/29/413041.html
+
     LocalDateTime localDateTime;
     // year month dayOfMonth(当前月的 第几天) hour minute second nanoOfsecond(当前秒的 多少纳秒)
     LocalDateTime definedLocalDateTime;
