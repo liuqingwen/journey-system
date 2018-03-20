e67bfde60e2b35e3e6861def961f940db4c6afd3
刘庆文 <lqingw1207@qq.com>
2018-03-20 14:52:41 +0800

    Java 8 Function

---
+++ journey-test/src/test/java/com/journey/stream/StreamTest.java
@@ -0,0 +1,20 @@
+package com.journey.stream;
+
+import org.junit.Test;
+
+/**
+ * @author liuqingwen
+ * @date 2018/3/20.
+ */
+public class StreamTest {
+
+    @Test
+    public void test() {
+
+        Object o = new Object();
+        double d = (double)o;
+
+
+    }
+
+}
