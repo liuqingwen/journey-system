79b62f93f6f4272d27db6cde68dcc09056d7488a
刘庆文 <lqingw1207@qq.com>
2018-04-17 13:08:55 +0800

    Java Pattern

---
+++ journey-test/src/test/java/com/journey/pattern/PatternTest.java
@@ -0,0 +1,23 @@
+package com.journey.pattern;
+
+import org.junit.Test;
+
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author liuqingwen
+ * @date 2018/4/17.
+ */
+public class PatternTest {
+
+    @Test
+    public void test() {
+
+        Pattern pattern = Pattern.compile("\\s+");
+        System.out.println(pattern.splitAsStream("liu qing wen is best good").collect(toList()));
+
+    }
+
+}
