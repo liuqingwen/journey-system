083ea19f96a92f8c4b315c19c7ac52edf5e50cc8
刘庆文 <lqingw1207@qq.com>
2018-04-28 14:13:33 +0800

    journey-base 提交

--- journey-test/src/test/java/com/journey/io/FileTest.java
+++ journey-test/src/test/java/com/journey/io/FileTest.java
@@ -3,10 +3,11 @@ package com.journey.io;
 import org.junit.Test;
 
 import java.io.*;
-import java.util.List;
-import java.util.Random;
-
-import static java.util.stream.Collectors.toList;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /**
  * @author liuqingwen
@@ -16,7 +17,9 @@ public class FileTest {
 
     String[] arrs = new String[]{"我", "叫", "胡", "汗", "三"};
     String zipFilePath = "/data/web/";
+    String fileName = "/data/web/test/user.txt";
     String zipFileName = "/data/web/user.zip";
+
 
     @Test
     public void test() {
@@ -26,7 +29,7 @@ public class FileTest {
             path.mkdirs();
         }
 
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(zipFileName), true)))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(fileName), true)))) {
             for (String arr : arrs) {
                 bufferedWriter.write(arr);
                 bufferedWriter.newLine();
@@ -41,9 +44,9 @@ public class FileTest {
     public void test2() {
 
 
-        File file = new File("/data/web/ImGroupMsg/75172378/201804/64.zip");
-        System.out.println(file.getAbsolutePath());
-        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)))){
+//        File file = new File("/data/web/ImGroupMsg/75172378/201804/64.zip");
+//        System.out.println(file.getAbsolutePath());
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)))){
 
             bufferedReader.lines().forEach(System.out::println);
 
@@ -58,13 +61,82 @@ public class FileTest {
     @Test
     public void test3() {
 
-        System.out.println(new File(zipFileName).exists());
-        System.out.println(new File(zipFileName).renameTo(new File("/data/web/user1.zip")));
-        System.out.println(new File(zipFileName).exists());
+        System.out.println(new File(fileName).exists());
+        System.out.println(new File(fileName).renameTo(new File("/data/web/user1.zip")));
+        System.out.println(new File(fileName).exists());
         System.out.println(new File("/data/web/user1.zip").exists());
-        System.out.println(new File("/data/web/user1.zip").renameTo(new File(zipFileName)));
-        System.out.println(new File(zipFileName).exists());
+        System.out.println(new File("/data/web/user1.zip").renameTo(new File(fileName)));
+        System.out.println(new File(fileName).exists());
+        System.getProperty("line.separator");
 
+    }
+
+    @Test
+    public void test4() {
+
+        try {
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFileName, true));
+            File pathFile = new File(zipFilePath);
+            Arrays.stream(pathFile.listFiles((dir, name) -> name.endsWith(".txt"))).forEach(file -> {
+                try {
+                    ZipEntry zipEntry = new ZipEntry(file.getAbsolutePath());
+                    zipOutputStream.putNextEntry(zipEntry);
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+                    String oneLine;
+                    while ((oneLine = bufferedReader.readLine()) != null) {
+                        zipOutputStream.write(oneLine.getBytes());
+                        zipOutputStream.write(System.getProperty("line.separator").getBytes());
+                    }
+                    zipOutputStream.flush();
+                    zipOutputStream.closeEntry();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void test5() {
+
+        try {
+            ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(zipFileName, true));
+            ZipEntry zipEntry = new ZipEntry("/data/web/test/");
+            zipOutputStream.putNextEntry(zipEntry);
+            zipOutputStream.closeEntry();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void test6() {
+
+        try {
+
+            ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(zipFileName));
+            ZipEntry nextEntry;
+            while ((nextEntry = zipInputStream.getNextEntry()) != null) {
+
+                System.out.println(nextEntry.getName());
+                Scanner scanner = new Scanner(zipInputStream);
+                while (scanner.hasNextLine()) {
+                    System.out.println(scanner.nextLine());
+                }
+
+                zipInputStream.closeEntry();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
