a29a2954df2c5c55061a19dd7156d95c7d61c3ed
刘庆文 <lqingw1207@qq.com>
2018-06-05 15:26:59 +0800

    binary-int 测试

--- journey-test/src/test/java/com/journey/test/Test.java
+++ journey-test/src/test/java/com/journey/test/Test.java
@@ -579,4 +579,29 @@ public class Test {
         }
 
     }
+
+    final static int MAXIMUM_CAPACITY = 1 << 30;
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    @org.junit.Test
+    public void test43() {
+        System.out.println(tableSizeFor(7));
+    }
+
+    @org.junit.Test
+    public void test44() {
+
+        System.out.println(Integer.toBinaryString(20));
+        System.out.println(Integer.toBinaryString(15));
+
+    }
 }
