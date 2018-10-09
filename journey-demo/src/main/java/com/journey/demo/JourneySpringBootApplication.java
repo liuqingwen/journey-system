42f1c218a18531b381c3b8b68ac67736410be145
liuqingwen <lqingw1207@qq.com>
2018-10-09 11:52:52 +0800

    项目基础构建

---
+++ journey-demo/src/main/java/com/journey/demo/JourneySpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.journey.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JourneySpringBootApplication.class, args);
+	}
+}
