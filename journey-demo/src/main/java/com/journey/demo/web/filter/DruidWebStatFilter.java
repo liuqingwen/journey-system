be0e1be8fcb7b564f65f7300d35e011e87aaf5c3
liuqingwen <lqingw1207@qq.com>
2018-10-10 14:33:58 +0800

    项目基础构建

--- journey-demo/src/main/java/com/journey/demo/web/filter/DruidWebStatFilter.java
+++ journey-demo/src/main/java/com/journey/demo/web/filter/DruidWebStatFilter.java
@@ -1,21 +1,27 @@
 package com.journey.demo.web.filter;
 
 import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.context.annotation.Configuration;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
 
 /**
  * @author liuqingwen
  * @date 2018/10/10.
  */
-@Configuration
 @WebFilter(urlPatterns = "/*", initParams = {
-        @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.jsp,/druid/*"),
+        @WebInitParam(name = "exclusions", value = "*.js,*.gif,*.jpg,*.png,*.css,*.ico,*.jsp,/druid/*,*.json"),
         @WebInitParam(name = "sessionStatMaxCount", value = "2000"),
         @WebInitParam(name = "sessionStatEnable", value = "true"),
         @WebInitParam(name = "profileEnable", value = "true")})
 public class DruidWebStatFilter extends WebStatFilter {
-
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        super.doFilter(request, response, chain);
+    }
 }
