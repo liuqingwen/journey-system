436ba0fb797053e6dfc719ccd38bf8d22e99d59f
liuqingwen <lqingw1207@qq.com>
2018-10-19 10:42:56 +0800

    Binary test

--- journey-test/src/test/java/com/linux/package-info.java
+++ journey-test/src/test/java/com/linux/package-info.java
@@ -1,4 +1,4 @@
-
+package com.linux;
 
 //1.压缩命令：
 //
@@ -13,3 +13,6 @@
 //　　命令格式：tar  -zxvf   压缩文件名.tar.gz
 //
 //　　解压缩后的文件只能放在当前的目录。
+
+// linux zip & unzip
+//http://www.cnblogs.com/chinareny2k/archive/2010/01/05/1639468.html
